b9320cf9e23570e5f71e517eb418c41fb7de9a72
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-07 16:04:57 +0900

    add title

--- ads/render.tsx
+++ ads/render.tsx
@@ -5,6 +5,7 @@ export default async function(data){
   const jsx = <html>
     <head>
       <meta charset="utf-8" />
+      <title>{data.title} - Ads by Scratch Ads</title>
     </head>
     <body>
       <div style="display: flex; flex-wrap: wrap;">
