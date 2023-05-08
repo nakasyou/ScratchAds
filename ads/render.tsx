8052b0fa629fe05579c92a761976a3f746a913b3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-08 21:02:16 +0900

    add background color

--- ads/render.tsx
+++ ads/render.tsx
@@ -27,6 +27,7 @@ export default async function(data,req){
           #seq{
             width: ${width}px;
             font-size: ${req.query("s")? req.query("s")+"px" : "1em"};
+            background-color: #fff;
           }
           #title{
             font-size: ${req.query("s")? req.query("s")*1.5+"px" : "1em"};
