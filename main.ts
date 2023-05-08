f52ed53a38d66c4666d2648397bf526dc6826832
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-08 19:57:22 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,22 @@ import { Hono } from "hono"
 import { serveStatic } from "hono/middleware"
 import { serve } from "std-serve"
 import ads from "./ads/index.ts"
+import * as dejs from "dejs"
 
 const app = new Hono()
 
 app.get('/ad/4-3',ads)
+app.use('/*', (c, next) => {
+  let pathname = new URL(c.req.url).pathname
+  if(pathname.at(-1) !== "/")
+    pathname += "/"
+  try{
+    return dejs.renderFileToString("."+pathname)
+  }catch try{
+    return dejs.renderFileToString("."+pathname+"index.ejs")
+  }catch
+    next()
+})
 app.use('/*',serveStatic({ root: "./static" }))
 
 serve(app.fetch)
