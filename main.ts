e8d4029f461ab7a4a9982e4dcce8fe7690830d2c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-08 20:25:13 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,23 +6,31 @@ import * as dejs from "dejs"
 
 const app = new Hono()
 
+const exists = async (filename: string): Promise<boolean> => {
+  try {
+    await Deno.stat(filename);
+    return true;
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      return false;
+    } else {
+      throw error;
+    }
+  }
+}
+
 app.get('/ad/4-3',ads)
 app.use('/*', async (c, next) => {
   let pathname = new URL(c.req.url).pathname
   if(pathname.at(-1) !== "/")
     pathname += "/"
-  try{
-    return c.html(await dejs.renderFileToString("./ejs"+pathname))
-  }catch(e){
-    if(!(e instanceof Deno.errors.NotFound))
-      throw e
-  }
-  try{
-    return c.html(await dejs.renderFileToString("./ejs"+pathname+"index.ejs"))
-  }catch(e){
-    if(!(e instanceof Deno.errors.NotFound))
-      throw e
-  }
+  let path = "./ejs"+pathname
+  if(exists(path))
+    return c.html(await dejs.renderFileToString(path))
+  path = "./ejs"+pathname+"index.ejs"
+  if(exists(path))
+    return c.html(await dejs.renderFileToString(path))
+
   await next()
 })
 app.use('/*',serveStatic({ root: "./static" }))
